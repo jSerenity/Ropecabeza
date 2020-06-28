3d73819ca3f07dc836e7eb83cf6d2ce801b42606
jpinzon <jpinzon@hypernovalabs.com>
2020-06-27 23:48:57 -0500

    creacion de botones diamicos

--- Garrido_016_Proy2.java
+++ Garrido_016_Proy2.java
@@ -6,18 +6,18 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import java.util.*;
- 
- 
 
 public class Garrido_016_Proy2 extends JFrame {
     private static final int ROWS = 4;
     private static final int COLS = 4;
     private int intentos = 0;
-    private static String[] presentation = {"Universidad Tecnologica de Panama", "Facultad de Ing. de Sistemas Computacionales", "Carrera: Desarrollo de Software", "Profesor: Ricardo Chan","Estudiante: Alba Garrido","Cedula: 7-709-2016","Grupo: ILS222","Fecha: 31 de Mayo de 2020"};
+    private static String[] presentation = { "Universidad Tecnologica de Panama",
+            "Facultad de Ing. de Sistemas Computacionales", "Carrera: Desarrollo de Software", "Profesor: Ricardo Chan",
+            "Estudiante: Alba Garrido", "Cedula: 7-709-2016", "Grupo: ILS222", "Fecha: 31 de Mayo de 2020" };
     Spy objSpy = new Spy();
     JButton verSpy;
-    private JLabel spyLabel = new JLabel(); 
-    private JLabel Intentos = new JLabel("intentos: 0"); 
+    private JLabel spyLabel = new JLabel();
+    private JLabel Intentos = new JLabel("intentos: 0");
     boolean DisplaySpy = false;
 
     public Garrido_016_Proy2() {
@@ -27,9 +27,8 @@ public class Garrido_016_Proy2 extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
- 
+
     protected void initComponents() {
-        
 
         final JPanel panelButton = new JPanel();
         final JPanel panelMandos = new JPanel(new FlowLayout());
@@ -37,64 +36,68 @@ public class Garrido_016_Proy2 extends JFrame {
         final JPanel panelInfo = new JPanel();
         panelInfo.setBackground(Color.LIGHT_GRAY);
         panelInfo.setBorder(new EmptyBorder(10, 10, 10, 10));
-        panelInfo.setLayout(new GridLayout(40,1));
+        panelInfo.setLayout(new GridLayout(40, 1));
         panelInfo.add(Intentos);
-
 
         JButton Start = new JButton("Iniciar");
         Start.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
-                
+
             }
         });
 
-        panelMandos.add(Start,BorderLayout.WEST);
+        panelMandos.add(Start, BorderLayout.WEST);
         panelMandos.setBorder(new EmptyBorder(10, 10, 10, 10));
         ActionListener buttonListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                mover(e.getSource());
             }
         };
 
-        
         panelButton.setLayout(null);
-        for(int y=ROWS; y>0; y--)
-         {   
-            for(int x=1; x<COLS; x++){
-                
-                    XYBotones button = new XYBotones(y+"",y,x);
-                    button.addActionListener(buttonListener);  
-                         
-                    panelButton.add(button);
-                    button.setBounds(y*50, x*50, 50, 50);
-                    panelButton.updateUI();
+        for (int y = ROWS; y > 0; y--) {
+            for (int x = 1; x < COLS; x++) {
+
+                XYBotones button = new XYBotones(y + "", y, x);
+                button.addActionListener(buttonListener);
+
+                panelButton.add(button);
+                button.setBounds(y * 50, x * 50, 50, 50);
+                panelButton.updateUI();
             }
         }
-                
 
-
-        panelpresen.setLayout(new GridLayout(20,2));
-        panelpresen.setAlignmentX(Component.CENTER_ALIGNMENT); 
+        panelpresen.setLayout(new GridLayout(20, 2));
+        panelpresen.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelpresen.setBackground(Color.LIGHT_GRAY);
         panelpresen.setBorder(new EmptyBorder(10, 10, 10, 10));
-        for(final String text: presentation)
-        {
-            final JLabel label = new JLabel(); 
-            label.setText(text);        
+        for (final String text : presentation) {
+            final JLabel label = new JLabel();
+            label.setText(text);
             panelpresen.add(label);
         }
         panelMandos.setBackground(Color.LIGHT_GRAY);
-        
+
         add(panelMandos, BorderLayout.NORTH);
         add(panelpresen, BorderLayout.WEST);
-        JPanel p = new JPanel(new BorderLayout()); 
-    
-        p.add(panelButton, BorderLayout.CENTER); 
-        add(p,BorderLayout.CENTER);
-        add(panelInfo,BorderLayout.EAST);
+        JPanel p = new JPanel(new BorderLayout());
+
+        p.add(panelButton, BorderLayout.CENTER);
+        add(p, BorderLayout.CENTER);
+        add(panelInfo, BorderLayout.EAST);
     }
-   
+
+    public void mover(Object b) {
+        var test = ((XYBotones) b);
+        var bx = ((XYBotones) b).getRow();
+        var by = ((XYBotones) b).getCol();
+        for (int x = bx * 50; x < 150; x++) 
+        {
+           test.setBounds(x, by*50, 50, 50);
+        }
+      
+    }
 
    
   
